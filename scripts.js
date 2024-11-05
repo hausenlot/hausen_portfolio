f70bf3d278678e72932365ddcfa720a276152c8c
hausenlot <petrehausen@gmail.com>
2024-11-06 00:34:42 +0800

    Upgraded CSS to Tailwind and Made the design responsive. Much bettter than first version

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
-function navigateTo(sectionId, element) {
-  const sections = document.querySelectorAll('.container');
-  sections.forEach(section => {
-      section.style.display = 'none';
-  });
+// function navigateTo(sectionId, element) {
+//   const sections = document.querySelectorAll('.container');
+//   sections.forEach(section => {
+//       section.style.display = 'none';
+//   });
 
-  const activeSection = document.getElementById(sectionId);
-  if (activeSection) {
-      activeSection.style.display = 'block';
-  }
+//   const activeSection = document.getElementById(sectionId);
+//   if (activeSection) {
+//       activeSection.style.display = 'block';
+//   }
 
-  const nav = document.querySelector('nav');
-  const animation = nav.querySelector('.animation');
-  const targetPosition = element.getBoundingClientRect();
-  const navPosition = nav.getBoundingClientRect();
+//   const nav = document.querySelector('nav');
+//   const animation = nav.querySelector('.animation');
+//   const targetPosition = element.getBoundingClientRect();
+//   const navPosition = nav.getBoundingClientRect();
 
-  animation.style.left = `${targetPosition.left - navPosition.left}px`;
-  animation.style.width = `${targetPosition.width}px`;
-}
+//   animation.style.left = `${targetPosition.left - navPosition.left}px`;
+//   animation.style.width = `${targetPosition.width}px`;
+// }
 
 emailjs.init("rAagqMof4QBT_39DR");
 
@@ -32,3 +32,35 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     });
 });
+
+const buttons = document.querySelectorAll('.nav-button');
+const containers = document.querySelectorAll('.content-container > div');
+
+function showContainer(target) {
+  containers.forEach(container => {
+    container.classList.add('hidden');
+  });
+  
+  const selectedContainer = document.querySelector(`.${target}`);
+  if (selectedContainer) {
+    selectedContainer.classList.remove('hidden');
+  }
+
+  buttons.forEach(button => {
+    button.classList.remove('active');
+  });
+
+  const activeButton = Array.from(buttons).find(button => button.getAttribute('data-target') === target);
+  if (activeButton) {
+    activeButton.classList.add('active');
+  }
+}
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const target = button.getAttribute('data-target');
+    showContainer(target);
+  });
+});
+
+showContainer('project-container');
